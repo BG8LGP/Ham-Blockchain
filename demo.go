56fb2ea89cc72a11f199d4ed1302e56c953dcb5c
BG8LGP <130383549+BG8LGP@users.noreply.github.com>
2023-06-06 23:50:37 +0800

    Create demo.go
    根据初步的一些的想法写出来的“伪”代码

---
+++ demo.go
@@ -0,0 +1,74 @@
+// Some Fake code here
+
+package main
+
+import (
+    "fmt"
+    "log"
+    "time"
+
+    "golang.org/x/crypto/sha3"
+    "golang.org/x/sync/errgroup"
+
+    ethcommon "github.com/ethereum/go-ethereum/common"
+    ethcrypto "github.com/ethereum/go-ethereum/crypto"
+    ethchain "github.com/ethereum/go-ethereum/core/types"
+    ethrpc "github.com/ethereum/go-ethereum/rpc"
+    "github.com/gorilla/websocket"
+)
+
+type AmateurRadioLog struct {
+    LogID      string `json:"log_id"`      //通联日志ID
+    Date       string `json:"date"`        //通联日期
+    Time       string `json:"time"`        //通联时间
+    Frequency  int    `json:"frequency"`   //通联频率（MHz）
+    Mode       string `json:"mode"`        //通联模式（例如，LSB，CW等）
+    Name       string `json:"name"`        //通联对方呼号
+    CallSign   string `json:"call_sign"`   //通联对方电台执照号码
+    Comments   string `json:"comments"`    //通联备注信息
+}
+
+func main() {
+    // 初始化以太坊节点和RPC客户端
+    node, err := ethrpc.NewDefaultNode("ws://localhost:8545", nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    client, err := ethrpc.NewClient(node.ClientConfig(), node)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // 创建WebSocket连接并订阅以太坊事件
+    ws, _, err := ethcommon.NewWSProvider(node)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer ws.Close()
+    eg := &errgroup.Group{}
+    defer eg.Wait()
+    for i := 0; i < 10; i++ {
+        eg.Go(func() error {
+            return ws.WriteJSON(&ethchain.BlockEvent{})
+        })
+    }
+
+    // 初始化日志记录器和管理器
+    loggers := make(map[string]*AmateurRadioLog)
+    lock := &sync.Mutex{}
+    manager := &日志记录器管理器{lock: lock, loggers: loggers}
+    go manager.run()
+
+    // 处理WebSocket消息循环，监听新交易并获取日志记录器的地址
+    for msg := range ws.ReadChan() {
+        event, ok := msg.(*ethchain.BlockEvent)
+        if !ok {
+            continue
+        }
+        hash := ethcommon.BytesToHash(event.Hash[:])
+        logs := manager.getLogs(hash)
+        for _, log := range logs {
+            fmt.Printf("%s: %s\n", log.Timestamp, log)
+        }
+    }
+}
